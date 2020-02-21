ef626796398392dec68d9f20af6a8a0b08c69a1f
woobin <kebee95@gmail.com>
2020-02-21 23:38:53 +0900

    0221

---
+++ BFS/불2.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <queue>
+#define MAX 1000
+using namespace std;
+
+struct yxc {
+	int y, x, c;
+};
+
+const int dy[] = { -1,1,0,0 }, dx[] = { 0,0,-1,1 };
+int r, c, ans;
+char map[MAX][MAX];
+bool check[MAX][MAX];
+bool fire[MAX][MAX];
+queue<yxc> jq;
+queue<yxc> fq;
+
+int bfs() {
+	
+	while (!jq.empty()) {
+		int jsize = jq.size();
+		int fsize = fq.size();
+
+		while (fsize--) {
+			int fy = fq.front().y;
+			int fx = fq.front().x;
+			fq.pop();
+			for (int dir = 0; dir < 4; dir++) {
+				int ny = fy + dy[dir];
+				int nx = fx + dx[dir];
+				if (ny<0 || ny>r - 1 || nx<0 || nx>c - 1) continue;
+				if (map[ny][nx] != '#' && !fire[ny][nx]) {
+					fq.push({ ny,nx,0 });
+					fire[ny][nx] = true;
+					map[ny][nx] = 'F';
+				}
+			}
+		}
+		while (jsize--) {
+			int cy = jq.front().y;
+			int cx = jq.front().x;
+			int ct = jq.front().c;
+			jq.pop();
+			for (int dir = 0; dir < 4; dir++) {
+				int ny = cy + dy[dir];
+				int nx = cx + dx[dir];
+				if (ny < 0 || ny > r - 1 || nx < 0 || nx > c - 1) {
+					return ct + 1;
+				}
+				if (map[ny][nx] == '.' && !check[ny][nx]) {
+					jq.push({ ny,nx,ct + 1 });
+					check[ny][nx] = true;
+				}
+			}
+		}
+
+	}
+	return -1;
+}
+
+int main() {
+	scanf("%d %d", &r, &c);
+	for (int i = 0; i < r; i++) {
+		scanf("%s", map[i]);
+		for (int j = 0; j < c; j++) {
+			if (map[i][j] == 'J') {
+				jq.push({ i,j,0 });
+				check[i][j] = true;
+			}
+			else if (map[i][j] == 'F') {
+				fq.push({ i,j,0 });
+				fire[i][j] = true;
+			}
+		}
+	}
+	ans = bfs();
+	if (ans == -1) printf("IMPOSSIBLE\n");
+	else printf("%d\n", ans);
+	return 0;
+}
---
+++ DFS/효율적인 해킹.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#define MAX 10001
+using namespace std;
+
+int n, m, ret;
+int maxs = 0;
+vector<int> v[MAX];
+int check[MAX];
+int counts[MAX];
+
+void dfs(int n) {
+	check[n] = true;
+	ret++;
+	for (int i = 0; i < v[n].size(); i++) {
+		if (!check[v[n][i]]) dfs(v[n][i]);
+	}
+}
+
+int main() {
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		v[b].push_back(a);
+	}
+
+	for (int i = 1; i <= n; i++) {
+		memset(check, 0, sizeof(check));
+		ret = 0;
+		dfs(i);
+		counts[i] = ret;
+		maxs = max(ret, maxs);
+	}
+	
+	for (int i = 1; i <= n; i++) {
+		if (counts[i] == maxs) printf("%d ", i);
+	}
+	return 0;
+}
