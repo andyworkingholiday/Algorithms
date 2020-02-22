2c81b441e6a1afcc5d51215e31454040d1fa3017
woobin <kebee95@gmail.com>
2020-02-22 16:47:58 +0900

    0222

---
+++ BFS/말이 되고픈 원숭이.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <queue>
+#define MAX 200
+using namespace std;
+
+struct monkey {
+	int y, x, c, h;
+};
+
+int k, w, h;
+int map[MAX][MAX];
+bool check[MAX][MAX][31];
+const int hy[] = { -1,-2,-2,-1,1,2,2,1 };
+const int hx[] = { -2,-1,1,2,2,1,-1,-2 };
+const int my[] = { -1,1,0,0 };
+const int mx[] = { 0,0,-1,1 };
+
+
+int bfs() {
+	queue<monkey> q;
+	q.push({ 0,0,0,0 });
+	check[0][0][0] = 1;
+	while (!q.empty()) {
+		int cy = q.front().y;
+		int cx = q.front().x;
+		int cc = q.front().c;
+		int ch = q.front().h;
+		q.pop();
+		if (cy == h - 1 && cx == w - 1) {
+			return cc;
+		}
+
+		if (ch < k) {
+			for (int dir = 0; dir < 8; dir++) {
+				int ny = cy + hy[dir];
+				int nx = cx + hx[dir];
+				if (ny<0 || ny>h - 1 || nx<0 || nx>w - 1) continue;
+				if (map[ny][nx] != 1 && !check[ny][nx][ch+1]) {
+					q.push({ ny,nx,cc + 1, ch + 1 });
+					check[ny][nx][ch+1] = true;
+				}
+			}
+		}
+
+
+		for (int dir = 0; dir < 4; dir++) {
+			int ny = cy + my[dir];
+			int nx = cx + mx[dir];
+			if (ny<0 || ny>h - 1 || nx<0 || nx>w - 1) continue;
+			if (map[ny][nx] != 1 && ! check[ny][nx][ch]) {
+				q.push({ ny,nx,cc + 1, ch});
+				check[ny][nx][ch] = true;
+			}
+		}
+	}
+
+	return -1;
+}
+
+int main() {
+	scanf("%d %d %d", &k, &w, &h);
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			scanf("%d", &map[i][j]);
+		}
+	}
+
+	printf("%d\n", bfs());
+	return 0;
+}
---
+++ BFS/뿌요뿌요.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <queue>
+#include <stack>
+#include <cstring>
+using namespace std;
+
+char map[12][6];
+bool check[12][6];
+const int dy[] = { -1,1,0,0 }, dx[] = { 0,0,-1,1 };
+queue<pair<int, int>> q;
+
+void dfs(int y, int x) {
+	check[y][x] = true;
+	q.push({ y,x });
+	for (int dir = 0; dir < 4; dir++) {
+		int ny = y + dy[dir];
+		int nx = x + dx[dir];
+		if (ny < 0 || ny>11 || nx < 0 || nx>5) continue;
+		if (map[ny][nx] == map[y][x] && !check[ny][nx]) {
+			dfs(ny, nx);
+		}
+	}
+}
+
+void sorting(char a[12][6]) {
+	for (int j = 0; j < 6; j++) {
+		stack<int> s;
+		for (int i = 0; i < 12; i++) {
+			if (a[i][j] != '.') {
+				s.push(a[i][j]);
+			}
+			a[i][j] = '.';
+		}
+
+		int idx = 11;
+		while (!s.empty()) {
+			a[idx][j] = s.top();
+			s.pop();
+			idx--;
+		}
+	}
+}
+
+int main() {
+	for (int i = 0; i < 12; i++)
+		scanf("%s", map + i);
+
+
+	int ret = 0;
+
+	bool flag = true;
+	while (1) {
+		flag = false;
+		memset(check, false, sizeof(check));
+		for (int i = 0; i < 12; i++) {
+			for (int j = 0; j < 6; j++) {
+				if (map[i][j] != '.' && !check[i][j]) {
+					dfs(i, j);
+					if (q.size()>=4) {
+						flag = true;
+						while (!q.empty()) {
+							map[q.front().first][q.front().second] = '.';
+							q.pop();
+						}
+					}
+					else {
+						while (!q.empty()) q.pop();
+					}
+
+				}
+			}
+		}
+
+		if (!flag) break;
+
+		sorting(map);
+		ret++;
+	}
+
+	printf("%d\n", ret);
+	return 0;
+}
