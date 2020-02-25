838def13a8254a463d37ce9c84a0e5de4a408a5c
woobin <kebee95@gmail.com>
2020-02-25 20:46:18 +0900

    0225 swea

---
+++ SWEA/시험감독.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#define MAX 1000000
+using namespace std;
+
+int testroom[MAX];
+int n, b, c;
+long long ret;
+
+int main() {
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", testroom + i);
+	}
+	scanf("%d %d", &b, &c);
+
+
+	for (int i = 0; i < n; i++) {
+		if (b >= testroom[i]) ret++;
+		else {
+			testroom[i] -= b;
+			ret++;
+			if (testroom[i] % c != 0) {
+				ret += ((testroom[i] / c) + 1);
+			}
+
+			else ret += testroom[i] / c;
+		}
+	}
+	printf("%lld\n", ret);
+	return 0;
+}
---
+++ SWEA/치킨배달_성공.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MAX  987654321
+using namespace std;
+
+int n, m, map[50][50], ans;
+int real_ans = MAX;
+vector<pair<int, int>> v;
+vector<pair<int, int>> chicken;
+vector<pair<int, int>> house;
+
+void pick_chick(int pos) {
+	if (chicken.size() == m) {
+		ans = 0;
+		for (int i = 0; i < house.size(); i++) {
+			int mins = MAX;
+			for (int j = 0; j < m; j++) {
+				int distance = abs(house[i].first - chicken[j].first) + abs(house[i].second - chicken[j].second);
+				mins = min(distance, mins);
+			}
+			ans += mins;
+		}
+
+		real_ans = min(ans, real_ans);
+		return;
+ 	}
+
+	for (int i = pos; i < v.size(); i++) {
+		chicken.push_back(v[i]);
+		pick_chick(i + 1);
+		chicken.pop_back();
+	}
+}
+
+
+int main() {
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			scanf("%d", &map[i][j]);
+			if (map[i][j] == 2) v.push_back({ i,j });
+			else if (map[i][j] == 1) house.push_back({ i,j });
+		}
+	}
+	pick_chick(0);
+	printf("%d\n", real_ans);
+	return 0;
+}
