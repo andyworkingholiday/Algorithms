9819022b89b408833fb1ca88baeda17815a1824e
woobin <kebee95@gmail.com>
2020-02-26 23:37:57 +0900

    0226

---
+++ SWEA/낚시왕.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <queue>
+#define MAX 100
+using namespace std;
+
+struct shark {
+	int s, d, z;
+};
+
+struct yx {
+	int y, x;
+};
+
+
+int r, c, m;
+shark map[MAX][MAX];
+const int dy[] = { -1,1,0,0 };
+const int dx[] = { 0,0,1,-1 };
+
+int main() {
+	scanf("%d %d %d", &r, &c, &m);
+	for (int i = 0; i < m; i++) {
+		int y, x, vel, direc, size;
+		scanf("%d %d %d %d %d", &y, &x, &vel, &direc, &size);
+		map[y - 1][x - 1] = { vel,direc - 1,size };
+	}
+
+	
+	int result = 0;
+	for (int index = 0; index < c; index++) {
+		for (int i = 0; i < r; i++) {
+			if (map[i][index].z > 0) {
+				result += map[i][index].z;
+				map[i][index] = { 0,0,0 };
+				break;
+			}
+		}
+
+		
+		queue<pair<shark, yx>> q;
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				if (map[i][j].z > 0) {
+					q.push({ map[i][j], {i,j} });
+					map[i][j] = { 0,0,0 };
+				}
+			}
+		}
+
+		while (!q.empty()) {
+			int y = q.front().second.y;
+			int x = q.front().second.x;
+			int size = q.front().first.z;
+			int dir = q.front().first.d;
+			int speed = q.front().first.s;
+			q.pop();
+			for (int s = 0; s < speed; s++) {
+				if (dir == 0 || dir == 1) {
+					int ny = y + dy[dir];
+					if (ny<0 || ny >r - 1)
+						dir = 1 - dir;
+					y += dy[dir];
+				}
+
+				else {
+					int nx = x + dx[dir];
+					if (nx<0 || nx>c - 1)
+						dir = 5 - dir;
+					x += dx[dir];
+				}
+			}
+
+
+			if (map[y][x].z > 0) {
+				if (map[y][x].z < size) {
+					map[y][x] = { speed,dir,size };
+				}
+			}
+
+			else
+				map[y][x] = { speed,dir,size };
+
+
+		}
+	}
+	printf("%d\n", result);
+	return 0;
+}
---
+++ SWEA/모의테스트/프로세서 연결하기.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+int map[12][12];
+int cores, length;
+const int dy[] = { -1,1,0,0 }, dx[] = { 0,0,-1,1 };
+vector<pair<int, int>> v;
+
+
+void counting(int idx, int core_count, int wire_count, int n) {
+	if (cores > v.size() - n + core_count) return;
+	if (n == v.size()) {
+		if (cores < core_count) {
+			cores = core_count;
+			length = wire_count;
+		}
+
+		else if (cores == core_count) {
+			if (length > wire_count) length = wire_count;
+		}
+		return;
+	}
+
+	int temp[12][12] = { 0, };
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			temp[i][j] = map[i][j];
+		}
+	}
+
+	for (int dir = 0; dir < 4; dir++) {
+		int ny = 0, nx = 0, cnt = 0;
+		int y = v[idx].first;
+		int x = v[idx].second;
+		bool flag = true;
+
+		while (1) {
+			ny = y + dy[dir];
+			nx = x + dx[dir];
+			if (ny<0 || ny>N - 1 || nx<0 || nx>N - 1) break;
+			if (map[ny][nx] == 1 || map[ny][nx] == 2) {
+				flag = false;
+				break;
+			}
+
+			cnt++;
+			map[ny][nx] = 2;
+			y = ny;
+			x = nx;
+		}
+
+		if (flag) {
+			counting(idx + 1, core_count + 1, wire_count + cnt, n + 1);
+		}
+
+		if (cnt != 0) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					map[i][j] = temp[i][j];
+				}
+			}
+		}
+	}
+
+	counting(idx + 1, core_count, wire_count, n + 1);
+}
+
+int main() {
+	int test;
+	scanf("%d", &test);
+	for(int x=1; x<=test; x++) {
+		memset(map, false, sizeof(map));
+		cores = 0, length = 0;
+		v.clear();
+		scanf("%d", &N);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				scanf("%d", &map[i][j]);
+				if (i == 0 || j == 0 || i == N - 1 || j == N - 1) continue;
+				if (map[i][j] == 1)
+					v.push_back({ i, j });
+			}
+		}
+
+		counting(0, 0, 0, 0);
+		printf("#%d %d\n", x, length);
+	}
+	return 0;
+}
