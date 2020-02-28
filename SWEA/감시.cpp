4c67e0ae55fe09772d4ba44981f3cc7aeb650535
woobin <kebee95@gmail.com>
2020-02-28 21:12:49 +0900

    0228

---
+++ SWEA/감시.cpp
@@ -0,0 +1,143 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+int N, M;
+int ans = 987654321;
+int map[8][8];
+vector<pair<int, int>> v;
+
+const int dy[] = { -1,0,1,0 };
+const int dx[] = { 0,1,0,-1 };
+
+void move(int dir, int y, int x) {
+	switch (dir) {
+	case 0:
+		for (int i = y - 1; i >= 0; i--) {
+			if (map[i][x] == 6) break;
+			if (map[i][x] == 0) map[i][x] = -1;
+		}
+		break;
+
+	case 1:
+		for (int j = x + 1; j < M; j++) {
+			if (map[y][j] == 6) break;
+			if (map[y][j] == 0) map[y][j] = -1;
+		}
+		break;
+
+	case 2:
+		for (int i = y + 1; i < N; i++) {
+			if (map[i][x] == 6) break;
+			if (map[i][x] == 0) map[i][x] = -1;
+		}
+		break;
+
+	case 3:
+		for (int j = x - 1; j  >= 0; j--) {
+			if (map[y][j] == 6) break;
+			if (map[y][j] == 0) map[y][j] = -1;
+		}
+		break;
+
+	}
+}
+
+void copy(int a[8][8], int b[8][8]) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			a[i][j] = b[i][j];
+		}
+	}
+}
+
+void dfs(int cnt) {
+	if (cnt == v.size()) {
+		int ret = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (map[i][j] == 0)
+					ret++;
+			}
+		}
+		ans = min(ans, ret);
+		if (ans == 0) {
+			printf("%d\n", ans);
+			exit(0);
+		}
+		return;
+	}
+
+
+	int temp[8][8] = { 0, };
+	copy(temp, map);
+
+	int y = v[cnt].first;
+	int x = v[cnt].second;
+	int number = map[y][x];
+
+	switch (number) {
+	case 1:
+		for (int dir = 0; dir < 4; dir++) {
+			move(dir, y, x);
+			dfs(cnt + 1);
+			copy(map,temp);
+		}
+		break;
+
+	case 2:
+		for (int dir = 0; dir < 2; dir++) {
+			move(dir, y, x);
+			move(dir + 2, y, x);
+			dfs(cnt + 1);
+			copy(map, temp);
+		}
+		break;
+
+	case 3:
+		for (int dir = 0; dir < 4; dir++) {
+			move(dir, y, x);
+			move((dir + 1) % 4, y, x);
+			dfs(cnt + 1);
+			copy(map, temp);
+		}
+		break;
+
+	case 4:
+		for (int dir = 0; dir < 4; dir++) {
+			move(dir, y, x);
+			move((dir + 1) % 4, y, x);
+			move((dir + 2) % 4, y, x);
+			dfs(cnt + 1);
+			copy(map, temp);
+		}
+		break;
+
+	case 5:
+		for (int dir = 0; dir < 4; dir++) {
+			move(dir, y, x);
+		}
+		dfs(cnt + 1);
+		copy(map, temp);
+		break;
+	}
+	return;
+}
+
+int main() {
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			scanf("%d", &map[i][j]);
+			if (map[i][j] != 0 && map[i][j] != 6) {
+				v.push_back({ i,j });
+			}
+		}
+	}
+
+	dfs(0);
+	printf("%d\n", ans);
+	return 0;
+}
---
+++ SWEA/새로운 게임2.cpp
@@ -0,0 +1,119 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+using namespace std;
+
+struct yxd{
+	int y, x, d;
+};
+
+int N, K;
+int turn = 1;
+vector<int> v[12][12];
+int map[12][12];
+bool flag = false;
+const int dy[] = { 0,0,-1,1 };
+const int dx[] = { 1,-1,0,0 };
+yxd horse[11];
+
+void move() {
+	while (1) {
+		if (turn > 10) {
+			turn = -1;
+			break;
+		}
+
+		for (int idx = 1; idx <= K; idx++) {
+			int cy = horse[idx].y;
+			int cx = horse[idx].x;
+			int cd = horse[idx].d;
+			int ny = cy + dy[cd];
+			int nx = cx + dx[cd];
+
+			if (ny <0 || ny>N - 1 || nx<0 || nx>N - 1 || map[ny][nx] == 2) {
+				if (cd % 2 == 0) cd += 1;
+				else cd -= 1;
+				ny = cy + dy[cd];
+				nx = cx + dx[cd];
+				horse[idx].d = cd;
+				if (ny <0 || ny>N - 1 || nx<0 || nx>N - 1 || map[ny][nx] == 2) {
+					ny = cy;
+					nx = cx;
+					continue;
+				}
+			}
+
+			if (map[ny][nx] == 0) {
+				stack<pair<int, int>> s;
+				while (v[cy][cx].back()!= idx) {
+					int num = v[cy][cx].back();
+					int dir = horse[num].d;
+					v[cy][cx].pop_back();
+					s.push({ num,dir });
+				}
+				int last = v[cy][cx].back();
+				s.push({ last, horse[last].d });
+				v[cy][cx].pop_back();
+
+				while (!s.empty()) {
+					int num = s.top().first;
+					int dir = s.top().second;
+					s.pop();
+					v[ny][nx].push_back(num);
+					horse[num] = { ny,nx, dir };
+				}
+			}
+
+			else if (map[ny][nx] == 1) {
+				queue<pair<int, int>> q;
+				while (v[cy][cx].back()!= idx) {
+					int num = v[cy][cx].back();
+					int dir = horse[num].d;
+					v[cy][cx].pop_back();
+					q.push({ num,dir });
+				}
+
+				int last = v[cy][cx].back();
+				q.push({ last, horse[last].d });
+				v[cy][cx].pop_back();
+
+				while (!q.empty()) {
+					int num = q.front().first;
+					int dir = q.front().second;
+					q.pop();
+					v[ny][nx].push_back(num);
+					horse[num] = { ny,nx,dir };
+				}
+
+			}
+
+			if (v[ny][nx].size() >= 4) {
+				return;
+			}
+		}
+		turn++;	
+	}
+	
+}
+
+int main() {
+	scanf("%d %d", &N, &K);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			scanf("%d", &map[i][j]);
+		}
+	}
+
+	for (int i = 1; i<= K; i++) {
+		int a, b, c;
+		scanf("%d %d %d", &a, &b, &c);
+		horse[i] = { a - 1,b - 1,c - 1 };
+		v[a - 1][b - 1].push_back(i);
+	}
+
+	int time = 0;
+	move();
+	printf("%d\n", turn);
+	return 0;
+}
