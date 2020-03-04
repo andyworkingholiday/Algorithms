03e17898d0d5d00e4bd6f03e247ff80b16909f0f
woobin <kebee95@gmail.com>
2020-03-04 17:59:07 +0900

    0304

---
+++ SWEA/모의테스트/요리사.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+int N, ret;
+int ans = 987654321;
+int map[16][16];
+bool arr[16];
+vector<int> t;
+vector<int> f;
+vector<int> rt;
+vector<int> rf;
+int a, b, c, d;
+
+void calculate(int pos, int cnt) {
+	if (cnt == 2) {
+		a += (map[rt[0]][rt[1]] + map[rt[1]][rt[0]]);
+		b += (map[rf[0]][rf[1]] + map[rf[1]][rf[0]]);
+		return;
+	}
+
+	for (int i = pos; i < t.size(); i++) {
+		rt.push_back(t[i]);
+		rf.push_back(f[i]);
+		calculate(i + 1, cnt + 1);
+		rt.pop_back();
+		rf.pop_back();
+	}
+
+}
+
+
+
+void bts(int pos, int cnt) {
+	if (cnt==N/2) {
+		t.clear();
+		f.clear();
+		a = 0, b = 0;
+		for (int i = 0; i < N; i++) {
+			if (arr[i]) t.push_back(i);
+			else f.push_back(i);
+		}		
+		calculate(0, 0);
+
+		if (c == b && d == a) {
+			return;
+		}
+
+		c = a, d = b;
+		ret = abs(a - b);
+		ans = min(ans, ret);
+		return;
+	}
+
+	for (int i = pos; i < N; i++) {
+		arr[i] = true;
+		bts(i + 1, cnt + 1);
+		arr[i] = false;
+	}
+}
+
+int main() {
+	int testcase;
+	scanf("%d", &testcase);
+	for (int iter = 1; iter <= testcase; iter++) {
+		memset(arr, false, sizeof(false));
+		ans = 987654321, ret = 0;
+		rt.clear(); rf.clear();
+		t.clear(); f.clear();
+		a = 0, b = 0, c = 0, d = 0;
+		scanf("%d", &N);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				scanf("%d", &map[i][j]);
+			}
+		}
+
+		bts(0, 0);
+		printf("#%d %d\n", iter, ans);
+	}
+	return 0;
+}
---
+++ SWEA/모의테스트/줄기세포배양.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <queue>
+#include <cstring>
+#define half 250
+using namespace std;
+
+struct cell {
+	int y, x, cnt, life;
+};
+
+int map[500][500];
+int N, M, K, test;
+const int dy[] = { -1,1,0,0 };
+const int dx[] = { 0,0,-1,1 };
+
+int main() {
+	int testcase;
+	scanf("%d", &testcase);
+	for (int iter = 1; iter <= testcase; iter++) {
+
+		scanf("%d %d %d", &N, &M, &K);
+		int ans = 0;
+		memset(map, 0, sizeof(map));
+
+		queue<cell> q[11];
+		for (int i = 250; i < 250 + N; i++) {
+			for (int j = 250; j < 250 + M; j++) {
+				scanf("%d", &map[i][j]);
+				if (map[i][j]) {
+					q[map[i][j]].push({ i,j,0,map[i][j] });
+				}
+			}
+		}
+
+		for (int i = 0; i <= K; i++) {
+			ans = 0;
+			for (int j = 10; j >= 1; j--) {
+				int size = q[j].size();
+				ans += size;
+				for (int idx = 0; idx < size; idx++) {
+					cell x = q[j].front(); q[j].pop();
+					if (x.cnt == 2 * x.life) {
+						ans--;
+						continue;
+					}
+
+
+					else if (x.cnt < x.life || x.cnt>x.life) {
+						q[j].push({ x.y, x.x, x.cnt + 1, x.life });
+					}
+
+					else if (x.cnt == x.life) {
+						q[j].push({ x.y, x.x, x.cnt + 1, x.life });
+						for (int dir = 0; dir < 4; dir++) {
+							int ny = x.y + dy[dir];
+							int nx = x.x + dx[dir];
+							if (!map[ny][nx]) {
+								map[ny][nx] = x.life;
+								q[j].push({ ny,nx,0,x.life });
+							}
+						}
+					}
+
+				}
+			}
+		}
+		printf("#%d %d\n", iter, ans);
+	}
+	return 0;
+}
