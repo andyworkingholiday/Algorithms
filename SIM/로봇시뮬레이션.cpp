49d0c4fb3cc011698fc6991fd5dfe4fd1f456adb
woobin <kebee95@gmail.com>
2020-03-10 00:02:25 +0900

    0310

---
+++ SIM/로봇시뮬레이션.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+struct command {
+	int a;
+	char d;
+	int b;
+};
+
+struct robot {
+	int y, x, d, n;
+};
+
+
+
+const int dy[] = { 0,1,0,-1 };
+const int dx[] = { 1,0,-1,0 };
+command commands[100];
+
+int A, B, N, M;
+int map[100][100];
+vector<robot> v;
+
+int change(char c) {
+	switch (c) {
+	case 'E':
+		return 0;
+	case 'S':
+		return 1;
+	case 'W':
+		return 2;
+	case 'N':
+		return 3;
+	}
+}
+
+bool move(int a, char c) {
+	robot r = v[a];
+	switch (c) {
+	case 'L':
+		v[a].d = (v[a].d + 3) % 4;
+		return 1;
+		break;
+
+	case 'R':
+		v[a].d = (v[a].d + 1) % 4;
+		return 1;
+		break;
+
+	case 'F':
+		int cy = r.y;
+		int cx = r.x;
+		int ny = cy + dy[r.d];
+		int nx = cx + dx[r.d];
+
+		if (ny <0 || ny> B - 1 || nx<0 || nx>A - 1) {
+			printf("Robot %d crashes into the wall\n", r.n);
+			return false;
+		}
+
+		else if (map[ny][nx] != 0) {
+			printf("Robot %d crashes into robot %d\n", r.n, map[ny][nx]);
+			return false;
+		}
+
+		else {
+			v[a].y = ny;
+			v[a].x = nx;
+			map[cy][cx] = 0;
+			map[ny][nx] = r.n;
+			return true;
+		}
+		break;
+	}
+}
+
+int main() {
+	bool flag = false;
+	scanf("%d %d", &A, &B); //a=5 b=4
+	scanf("%d %d", &N, &M);
+	v.push_back({ 0,0,0 });
+	for (int i = 1; i <= N; i++) {
+		int a, b;
+		char c;
+		scanf("%d %d %c", &a, &b, &c);
+		map[B - b][a - 1] = i;
+		v.push_back({ B - b, a - 1, change(c), i});
+	}
+
+	for (int i = 0; i < M; i++) {
+		int a, b;
+		char d;
+		scanf("%d %c %d", &a, &d, &b);
+		commands[i] = { a,d,b };
+	}
+
+	for (int i = 0; i < M; i++) {
+		int a = commands[i].a;
+		char d = commands[i].d;
+		int b = commands[i].b;
+		while (b--) {
+			if (!move(a, d)) {
+				flag = true;
+				break;
+			}
+		}
+
+		if (flag) break;
+	}
+
+	if (!flag) printf("OK\n");
+	return 0;
+}
---
+++ SWEA/원판 돌리기.cpp
@@ -0,0 +1,127 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+int N, M, T, k, cnt;
+int map[50][52];
+bool check[50][52];
+bool flag;
+const int dy[] = { -1,1,0,0 };
+const int dx[] = { 0,0,-1,1 };
+double sum;
+
+void rotate(int x, int dir, int cnt) {
+	if (dir == 0) {
+		while (cnt--) {
+			for (int i = 0; i < N; i++) {
+				if ((i + 1) % x == 0) {
+					for (int j = M; j >=1; j--) {
+						map[i][j + 1] = map[i][j];
+					}
+					map[i][1] = map[i][M + 1];
+				}
+			}
+		}
+	}
+
+	else {
+		while (cnt--) {
+			for (int i = 0; i < N; i++) {
+				if ((i + 1) % x == 0) {
+					for (int j = 0; j < M; j++) {
+						map[i][j] = map[i][j + 1];
+					}
+					map[i][M] = map[i][0];
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		map[i][M + 1] = map[i][1];
+		map[i][0] = map[i][M];
+	}
+
+}
+
+void dfs(int y, int x) {
+	check[y][x] = true;
+	for (int dir = 0; dir < 4; dir++) {
+		int ny = y + dy[dir];
+		int nx = x + dx[dir];
+		if (ny<0 || ny>N - 1 || nx <0 || nx>M + 1) continue;
+		if (!check[ny][nx] && (map[ny][nx] == k)) {
+			map[y][x] = 0;
+			map[ny][nx] = 0;
+			dfs(ny, nx);
+			flag = true;
+		}
+	}
+}
+
+int main() {
+	scanf("%d %d %d", &N, &M, &T);
+	for (int i = 0; i < N; i++) {
+		for (int j = 1; j <= M; j++) {
+			scanf("%d", &map[i][j]);
+		}
+	}
+
+	while (T--) {
+		int a, b, c;
+		scanf("%d %d %d", &a, &b, &c);
+		rotate(a, b, c);
+		memset(check, false, sizeof(check));
+		flag = false, sum = 0, cnt = 0;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 1; j <= M; j++) {
+				if (!check[i][j] && map[i][j]) {
+					k = map[i][j];
+					dfs(i, j);
+				}
+			}
+		}
+
+		if (!flag) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 1; j <= M; j++) {
+					if (map[i][j]) {
+						sum += map[i][j];
+						cnt++;
+					}
+				}
+			}
+
+			sum = sum / cnt;
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 1; j <= M; j++) {
+					if (map[i][j]) {
+						if (double(map[i][j]) > sum) {
+							map[i][j] -= 1;
+						}
+
+						else if (double(map[i][j]) < sum) {
+							map[i][j] += 1;
+						}
+
+					}
+				}
+			}
+
+		}
+	}
+
+	k = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 1; j <= M; j++) {
+			if (map[i][j]) {
+				k += map[i][j];
+			}
+			
+		}
+	}
+	printf("%d\n", k);
+	return 0;
+}
