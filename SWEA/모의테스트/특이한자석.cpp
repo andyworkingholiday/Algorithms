78c05d1aaa3b4f39b66b3122eb9c42b0422f60c0
woobin <kebee95@gmail.com>
2020-05-02 00:26:07 +0900

    0502

---
+++ SWEA/모의테스트/특이한자석.cpp
@@ -0,0 +1,117 @@
+﻿#include <iostream>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+int K, map[4][9], magnet[4], ans;
+
+void clock_right(int i) {
+	for (int j = 7; j >=0; j--) {
+		map[i][j + 1] = map[i][j];
+	}
+	map[i][0] = map[i][8];
+}
+
+void banclock_left(int i) {
+	map[i][8] = map[i][0];
+	for (int j = 0; j <= 7; j++) {
+		map[i][j] = map[i][j + 1];
+	}
+}
+
+void rotate(int i, int dir) {
+	switch (dir) {
+	case 1:
+		clock_right(i);
+		break;
+
+	case -1:
+		banclock_left(i);
+		break;
+	}
+}
+
+int main() {
+	int testcase;
+	scanf("%d", &testcase);
+	for (int t = 1; t <= testcase; t++) {
+		ans = 0;
+		scanf("%d", &K);
+		memset(map, 0, sizeof(map));
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 8; j++) {
+				scanf("%d", &map[i][j]);
+			}
+		}
+
+		while (K--) {
+			memset(magnet, 0, sizeof(magnet));
+			int a, b;
+			scanf("%d %d", &a, &b);
+			a--;
+			magnet[a] = b;
+
+			if (a == 0) {
+				if (map[0][2] != map[1][6]) {
+					magnet[1] = 0 - b;
+					if (map[1][2] != map[2][6]) {
+						magnet[2] = b;
+						if (map[2][2] != map[3][6]) {
+							magnet[3] = 0 - b;
+						}
+					}
+				}
+			}
+
+			if (a == 1) {
+				if (map[0][2] != map[1][6]) magnet[0] = 0 - b;
+				if (map[1][2] != map[2][6]) {
+					magnet[2] = 0 - b;
+					if (map[2][2] != map[3][6]) {
+						magnet[3] = b;
+					}
+				}
+			}
+
+			if (a == 2) {
+				if (map[2][2] != map[3][6]) magnet[3] = 0 - b;
+				if (map[1][2] != map[2][6]) {
+					magnet[1] = 0 - b;
+					if (map[1][6] != map[0][2]) {
+						magnet[0] = b;
+					}
+				}
+			}
+
+
+			if (a == 3) {
+				if (map[3][6] != map[2][2]) {
+					magnet[2] = 0 - b;
+					if (map[2][6] != map[1][2]) {
+						magnet[1] = b;
+						if (map[1][6] != map[0][2]) {
+							magnet[0] = 0 - b;
+						}
+					}
+				}
+			}
+
+			for (int i = 0; i < 4; i++) {
+				if (magnet[i] == 1 || magnet[i] == -1)
+					rotate(i, magnet[i]);
+			}
+
+
+
+		}
+
+		for (int i = 0; i < 4; i++) {
+			if (map[i][0] == 1) {
+				ans += pow(2, i);
+			}
+		}
+
+		printf("#%d %d\n", t, ans);
+	}
+	return 0;
+}
---
+++ SWEA/모의테스트/홈방범서비스.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+int N, M, house, K, cnt, ans;
+int map[20][20];
+
+void draw(int y, int x, int k) {
+	cnt = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (abs(i - y) + abs(j - x) < k) {
+				if (map[i][j] == 1) {
+					cnt++;
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	int test;
+	scanf("%d", &test);
+	for (int t = 1; t <= test; t++) {
+		ans = 0, cnt = 0, house = 0;
+		memset(map, 0, sizeof(map));
+		scanf("%d %d", &N, &M);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				scanf("%d", &map[i][j]);
+				if (map[i][j] == 1) house++;
+			}
+		}
+
+		K = 46;
+
+		for (int i = 1; i <= K; i++) {
+			if (i * i + (i - 1) * (i - 1) >= M * house) {
+				K = i;
+				break;
+			}
+		}
+
+		for (int k = K; k >= 1; k--) {
+			int money = k * k + (k - 1) * (k - 1);
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					draw(i, j, k);
+					if (money <= cnt * M) {
+						ans = max(ans, cnt);
+					}
+				}
+			}
+
+		}
+
+		printf("#%d %d\n", t, ans);
+	}
+
+	return 0;
+}
